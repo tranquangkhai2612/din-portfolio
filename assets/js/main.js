d8708def69d8b89fceadd64eab21d243bb258600
tranquangkhai2612 <tranquangkhai2612@gmail.com>
2023-04-10 11:56:36 +0700

    qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,77 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 
-
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
-
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
-
-
+const navLink = document.querySelectorAll(".nav_link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
 
+function toggleSkills() {
+  let itemClass = this.parentNode.classList;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
+}
 
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.CDATA_SECTION_NODE.target);
 
+    tabContents.forEach((tabCOntent) => {
+      tabContents.classList.remove("quali_active");
+    });
+
+    target.classList.add("quali_active");
+
+    tab.forEach((tab) => {
+      tab.classList.remove("quali_active");
+    });
+
+    tab.classList.add("quali_active");
+  });
+});
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
