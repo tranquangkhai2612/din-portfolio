9ec10298d96ec09ed748dbe1fe1a9cf6cb7e45ef
tranquangkhai2612 <tranquangkhai2612@gmail.com>
2023-04-11 15:44:16 +0700

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,21 +7,21 @@ const navClose = document.getElementById("nav-close");
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
+    navMenu.classList.add("show_menu");
   });
 }
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
+    navMenu.classList.remove("show_menu");
   });
 }
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav_link");
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
+  navMenu.classList.remove("show_menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
@@ -29,7 +29,7 @@ const skillsContent = document.getElementsByClassName("skills_content"),
   skillsHeader = document.querySelectorAll(".skills_header");
 
 function toggleSkills() {
-  let itemClass = this.parentNode.classList;
+  let itemClass = this.parentNode.className;
   for (i = 0; i < skillsContent.length; i++) {
     skillsContent[i].className = "skills_content skills_close";
   }
@@ -41,29 +41,51 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener("click", toggleSkills);
 });
+
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll("[data-target]"),
   tabContents = document.querySelectorAll("[data-content]");
 
 tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.CDATA_SECTION_NODE.target);
+    const target = document.querySelector(tab.dataset.target);
 
-    tabContents.forEach((tabCOntent) => {
-      tabContents.classList.remove("quali_active");
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("quali_active");
     });
 
     target.classList.add("quali_active");
 
-    tab.forEach((tab) => {
+    tabs.forEach((tab) => {
       tab.classList.remove("quali_active");
     });
 
     tab.classList.add("quali_active");
   });
 });
-/*==================== SERVICES MODAL ====================*/
 
+/*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll(".services_modal");
+const modalBtns = document.querySelectorAll(".services_button");
+const modalCloses = document.querySelectorAll(".services_modal-close");
+
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 /*==================== TESTIMONIAL ====================*/
