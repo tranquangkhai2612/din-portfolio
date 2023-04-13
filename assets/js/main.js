e732f7460de74c4ef0d018714186fb4b1a367273
tranquangkhai2612 <tranquangkhai2612@gmail.com>
2023-04-13 10:41:48 +0700

    project

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,6 +87,18 @@ modalCloses.forEach((modalClose) => {
   });
 });
 /*==================== PORTFOLIO SWIPER  ====================*/
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
